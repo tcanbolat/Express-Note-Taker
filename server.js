1d78bb039808d9e2616e101f336dfce7bf147529
atcanbolat <kangalcanbolat@gmail.com>
2020-01-12 01:58:52 -0800

    created another route to the db.json file containing note data and created post method that currently adds a new object from the req.body to the db.json file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
 // =============================================================
@@ -15,13 +16,52 @@ app.use(express.json());
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
+// route that sends the user to the main page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// route that sends the user to the notes page
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Displays all notes
+app.get("/api/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// Create New Notes - takes in JSON input
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newNote
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  // newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newNote);
+
+  fs.readFile('./db/db.json', function(err, data) {
+    if (err) throw err
+  
+    var arrayOfObjects = JSON.parse(data)
+    arrayOfObjects.push(newNote)
+
+    console.log(arrayOfObjects)
+
+
+    fs.writeFile('./db/db.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+      if (err) throw err
+      console.log('Done!')
+    })
+  
+  })
+
+
+
+  res.json(newNote);
 });
 
 // Starts the server to begin listening
