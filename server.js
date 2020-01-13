a5a75b98c5c7fd17efbd352b6969b6bd505538d6
atcanbolat <kangalcanbolat@gmail.com>
2020-01-13 15:35:48 -0800

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,5 +25,5 @@ require("./routes/apiDeleteRoute")(app);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log(`Our app is running on port ${ PORT }`);
 });
